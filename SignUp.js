057be013e5b20256edf580cb805c0450c9967a35
Lamyaa315 <lamyaalnahdi2@gmail.com>
2025-01-24 23:00:46 +0300

    mn

---
+++ SignUp.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    function showForm() {
+        const selectedRole = document.querySelector('input[name="role"]:checked').value;
+        const patientForm = document.getElementById('patient-form');
+        const doctorForm = document.getElementById('doctor-form');
+
+        if (selectedRole === 'patient') {
+            patientForm.classList.remove('hidden');
+            doctorForm.classList.add('hidden');
+        } else if (selectedRole === 'doctor') {
+            doctorForm.classList.remove('hidden');
+            patientForm.classList.add('hidden');
+        }
+    }
+
+    // Call showForm once to display the correct form by default
+    showForm();
+
+    // Attach event listeners to the radio buttons
+    const radioButtons = document.querySelectorAll('input[name="role"]');
+    radioButtons.forEach(radio => {
+        radio.addEventListener('change', showForm);
+    });
+});
